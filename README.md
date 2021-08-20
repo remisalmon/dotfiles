c6c44bd2dbbc1c221e5589fba96b40b7f60f58ad
Remi Salmon <salmon.remi@gmail.com>
2021-08-19 22:21:39 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# dotfiles.fish
+
+A [fish](https://fishshell.com) script to backup/restore my dotfiles.
+
+Do not try this at home.
+
+```shell
+> ./dotfiles.fish
+usage: dotfiles.fish backup|restore|clean
+```
---
+++ dotfiles.fish
@@ -0,0 +1,26 @@
+#!/usr/bin/env fish
+
+if test (count $argv) -eq 0 || not contains $argv[1] 'backup' \
+                                                     'restore' \
+                                                     'clean'
+    echo 'usage: dotfiles.fish backup|restore|clean'
+    exit 1
+end
+
+if test $argv[1] = 'clean'
+    rm -r -f -v dotfiles && mkdir dotfiles
+    exit
+end
+
+source env.fish
+
+for dotfile in $dotfiles
+    set path (string replace ~ '' (dirname $dotfile))
+
+    if test $argv[1] = 'backup'
+        mkdir -p dotfiles$path
+        cp -i -p -v $dotfile dotfiles$path
+    else if test $argv[1] = 'restore'
+        cp -i -p -v dotfiles$path/(basename $dotfile) $dotfile
+    end
+end
