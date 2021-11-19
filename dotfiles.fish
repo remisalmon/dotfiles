bf85e0fa029e05547f59a2d2bac45e97d43ffa54
Remi Salmon <salmon.remi@gmail.com>
2021-11-18 22:29:51 -0700

    Add diff

--- dotfiles.fish
+++ dotfiles.fish
@@ -1,5 +1,14 @@
 #!/usr/bin/env fish
 
+function diff
+    if not test -e $argv[1]
+    or not test -e $argv[2]
+        return
+    end
+
+    git diff --no-index $argv
+end
+
 if test (count $argv) -eq 0
 or not contains $argv[1] 'backup' \
                          'restore' \
@@ -23,12 +32,14 @@ for dotfile in $dotfiles
 
     if test $argv[1] = 'backup'
     and test -e $dotfile
+        diff dotfiles{$path}/{$file} $dotfile
         mkdir -p dotfiles{$path}
         cp -i -p -v $dotfile dotfiles{$path}
 
     else if test $argv[1] = 'restore'
     and test -e ~/{$path}
     and test -e dotfiles{$path}/{$file}
+        diff $dotfile dotfiles{$path}/{$file}
         cp -i -p -v dotfiles{$path}/{$file} $dotfile
     end
 end
