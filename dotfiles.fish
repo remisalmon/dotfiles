75de6246817263fdf54c6bf9c7e582821b1afb7f
Remi Salmon <salmon.remi@gmail.com>
2021-11-18 22:05:18 -0700

    Fix when restoring missing dotfile

--- dotfiles.fish
+++ dotfiles.fish
@@ -1,14 +1,17 @@
 #!/usr/bin/env fish
 
-if test (count $argv) -eq 0 || not contains $argv[1] 'backup' \
-                                                     'restore' \
-                                                     'clean'
+if test (count $argv) -eq 0
+or not contains $argv[1] 'backup' \
+                         'restore' \
+                         'clean'
     echo 'usage: dotfiles.fish backup|restore|clean'
     exit 1
 end
 
 if test $argv[1] = 'clean'
-    rm -r -f -v dotfiles && mkdir dotfiles
+    if test -s dotfiles/
+        rm -r -f -v dotfiles/.*
+    end
     exit
 end
 
@@ -16,11 +19,16 @@ source env.fish
 
 for dotfile in $dotfiles
     set path (string split -f 2 ~ (dirname $dotfile))
+    set file (basename $dotfile)
 
-    if test $argv[1] = 'backup' && test -e $dotfile
+    if test $argv[1] = 'backup'
+    and test -e $dotfile
         mkdir -p dotfiles{$path}
         cp -i -p -v $dotfile dotfiles{$path}
-    else if test $argv[1] = 'restore' && test -e ~/{$path}
-        cp -i -p -v dotfiles{$path}/(basename $dotfile) $dotfile
+
+    else if test $argv[1] = 'restore'
+    and test -e ~/{$path}
+    and test -e dotfiles{$path}/{$file}
+        cp -i -p -v dotfiles{$path}/{$file} $dotfile
     end
 end
